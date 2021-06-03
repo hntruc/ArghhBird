00b2ee805d911ebd3271380b0bc0324e5a429f53
[nt2505] <77818979+nt2505@users.noreply.github.com>
2021-06-03 22:30:10 +0700

    First version

---
+++ Bird/Assets/Scripts/Bird.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] float _launchForce = 500;
+    [SerializeField] float _maxDis = 2; //meters
+
+    Vector2 _startPosition;
+    Rigidbody2D _rigidbody2D;
+    SpriteRenderer _spriteRenderer;
+
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = _rigidbody2D.position;
+        _rigidbody2D.isKinematic = true;
+    }
+
+    void OnMouseDown()
+    {
+        _spriteRenderer.color = Color.cyan;
+    }
+
+    void OnMouseUp()
+    {
+        Vector2 currentPosition = _rigidbody2D.position;
+        Vector2 direction = _startPosition - currentPosition;
+        direction.Normalize();
+
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.AddForce(direction * _launchForce);
+
+        _spriteRenderer.color = Color.white;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 curPosition = mousePosition;
+
+        float dis = Vector2.Distance(curPosition, _startPosition);
+        if (dis > _maxDis)
+        {
+            Vector2 dir = curPosition - _startPosition;
+            dir.Normalize();
+            curPosition = _startPosition + dir * _maxDis;
+        }
+        if (curPosition.x > _startPosition.x)
+            curPosition.x = _startPosition.x;
+
+        _rigidbody2D.position = curPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(ResetAfterDelay());
+    }
+
+    IEnumerator ResetAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
+        _rigidbody2D.position = _startPosition;
+        _rigidbody2D.isKinematic = true;
+        _rigidbody2D.velocity = Vector2.zero;
+    }
+}
